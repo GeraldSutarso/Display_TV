160322c8c6f02c406c028d2fdfd26917d3637af9
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2024-12-13 14:24:02 +0700

    Update DB

---
+++ display/database/migrations/2024_12_13_070738_create_dosen_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->id(); // Kolom ID
+            $table->string('nama_dosen'); // Kolom nama_dosen
+            $table->string('foto_dosen')->nullable(); // Kolom foto_dosen (nullable jika opsional)
+            $table->timestamps(); // Kolom created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosen');
+    }
+};
+
